0dc8e4be0601c92afb13784a2faf9c9c42328ccc
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-03-07 16:56:30 -0700

    set up common static variables for DefinitionQueueTest and added passing test for testGetLastDef

--- src/main/test/com.ple.DefTree/DefinitionQueueTest.java
+++ src/main/test/com.ple.DefTree/DefinitionQueueTest.java
@@ -1,15 +1,27 @@
 package com.ple.DefTree;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class DefinitionQueueTest {
+
+  public final DefinitionQueue dq = new DefinitionQueue();
+  public final String testDef = "test - something used to ascertain that a certain property or behavior is working as expected.";
+  public final String testDef2 = "ascertain - to verify whether or not something is true.";
+
   @Test
   void testAddDef() {
-    DefinitionQueue definitionQueue = new DefinitionQueue();
-    String testDef = "test - something used to ascertain that a certain property or behavior is working as expected.";
-    definitionQueue.addDef(testDef);
-    assertEquals(testDef, definitionQueue.getLastDef());
+    dq.addDef(testDef);
+    assertEquals(testDef, dq.getLastDef());
   }
+
+  @Test
+  void testGetLastDef() {
+    dq.addDef(testDef2);
+    assertEquals(testDef2, dq.getLastDef());
+  }
+
 }
